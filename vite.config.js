6a8ac7710105263931e2909fb564777086a8b381
Cino Lee <cino@Cinos-Device.local>
2023-06-05 09:07:18 +0700

    update config

--- vite.config.js
+++ vite.config.js
@@ -1,47 +1,46 @@
 import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react-swc'
+import react from '@vitejs/plugin-react'
 import path from 'path'
 import fs from 'fs/promises'
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  // esbuild: {
-  //   loader: 'jsx',
-  //   // include: /src\/.*\.js/,
-  //   // include: /src\/.*\.jsx?$/,
-  //   // loader: "tsx",
-  //   // include: /src\/.*\.[tj]sx?$/,
-  //   exclude: [],
-  // },
-  // optimizeDeps: {
-  //   esbuildOptions: {
-  //     loader: {
-  //       '.js': 'jsx',
-  //     },
-  //   },
-  // },
-  esbuild: { loader: 'jsx', include: /src\/.*\.jsx?$/, exclude: [] },
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+    // loader: "tsx",
+    // include: /src\/.*\.[tj]sx?$/,
+    exclude: [],
+  },
   optimizeDeps: {
     esbuildOptions: {
       plugins: [
         {
-          name: 'load-js-files-as-jsx',
+          name: "load-js-files-as-jsx",
           setup(build) {
-            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => {
-              return {
-                loader: 'jsx',
-                contents: await fs.readFile(args.path, 'utf8'),
-              }
-            })
+            build.onLoad({ filter: /src\/.*\.js$/ }, async (args) => ({
+              loader: "jsx",
+              contents: await fs.readFile(args.path, "utf8"),
+            }));
           },
         },
       ],
     },
   },
+  // build: {
+  //   rollupOptions: {
+  //     input: {
+  //       app: './index.html', // default
+  //     },
+  //   },
+  // },
+  build: {
+    cssCodeSplit: true,
+  },
   resolve: {
     alias: {
       '@': path.resolve(__dirname, './src'),
     },
-  },
+  }
 })
